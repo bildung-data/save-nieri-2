ce0c9f39a0a4e6cd0de674270db083c78b32ce3c
bildung-data <133611450+bildung-data@users.noreply.github.com>
2023-05-15 22:47:25 -0300

    Update script.js - volvi todo para atras a la version de anoche - pre text input

--- script.js
+++ script.js
@@ -6,35 +6,6 @@ $(document).ready(function() {
     var catIsOnHisWayDown = false;
     var score = 0;
     var highscore = 0;
-    var popupShownBefore = null;
-    
-     // Check if the pop-up has been shown before
-        var popupShownBefore = localStorage.getItem('popupShownBefore');
-
-        // If the pop-up hasn't been shown before, display it
-          if (!popupShownBefore) {
-            $('#popupContainer').fadeIn();
-            console.log("popupContainer Fade In xq no se habia mostrado");
-            return;
-            
-          }
-
-        function hidepopupContainer() {
-          console.log("hidepopupContainer function está corriendo");
-
-         // var name = $('#nameInput').val();
-
-          // Store the entered name in cookies or local storage
-          //localStorage.setItem('playerName', name);
-
-          // Hide the pop-up
-          $('#popupContainer').fadeOut();
-
-        // Set to a value other than null
-            popupShownBefore = true; 
-          }
-
-     $('#enterNameButton').on('click', hidepopupContainer);
 
     // start (or restart) the game
     function startGame() {
@@ -53,7 +24,6 @@ $(document).ready(function() {
         $('#jumpButton').fadeIn();
         $('#startButton').off('click', startGame);
         $('#startButton, #instructions').hide();
-
         $('h2').css('visibility', 'hidden');
         $('.catContainer').removeClass('homeScreen');
         $('.catContainerOuter, .brickContainer').animate({
@@ -96,10 +66,8 @@ $(document).ready(function() {
     $(document).on('keydown', handleKeyPress);
     
     
-    
     // add an event listener to the jump button to handle the jump action - Guro!
     $('#jumpButton').on('click', handleKeyPress);
-
 
 
     // send a new brick from the left or right of the screen and move the stack down
